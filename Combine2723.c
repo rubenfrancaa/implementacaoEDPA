b59720b573db6b3d7fbda9b23f3cc2af90dcc236
yurikfernandes <56099546+yurikfernandes@users.noreply.github.com>
2020-08-17 17:10:59 -0300

    Update Combine2723.c

--- Combine2723.c
+++ Combine2723.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct noh {
     int somaEsq;
     int somaDir;
@@ -12,7 +11,7 @@ typedef struct noh *TREEh;
 //Definindo funções
 void PesqIn(TREEh arv);
 void PesqLeaf(TREEh arv);
-int Verifica(TREEh arv);
+int Verifica(TREEh arv, int total);
 
 void criaArvore(TREEh *arv, int vr);
 void insArvore(TREEh *arv, int vr);
@@ -22,7 +21,7 @@ void insArvoreDir(TREEh *arv, int vr, int esqPai, int dirPai);
 //Programa principal
 int main() {
     TREEh arvore = NULL;
-    int testes, quantPresentes, i, pesoPresente;
+    int testes, quantPresentes, i, pesoPresente, total, resposta;
     scanf("%d", &testes);
 
     //Laço para rodar o programa a quantidade de testes escolhida
@@ -30,6 +29,7 @@ int main() {
 
         //Resetando árvore toda vez que inica um teste e definindo valor 0.
         arvore = NULL;
+        total = 0;
 
         //Cria a árvore com raíz zero.
         criaArvore(&arvore, 0);
@@ -44,25 +44,74 @@ int main() {
             //Recebe os valores do presente a cada iteração.
             scanf("%d", &pesoPresente);
             insArvore(&arvore, pesoPresente);
+            total += pesoPresente;
+
+            resposta = Verifica(arvore, total);
+
+            PesqIn(arvore);
+
+            if(resposta == 1){
+                printf("Hohoho!\n");
+            }
+
         }
 
-        //printf("Contemple os nós folhas.\n");
+        printf("Feliz Natal!\n");
 
-        //PesqLeaf(arvore);
-        
-        //Pensar em alguma função pra verificar as possibilidades
-        if(Verifica(arvore) == 1){
-            printf("É tetra!");
-        }
-        else{
-            printf("Caiu!");
-        }
+
+        printf("\n");
+
+//        printf("Contemple os nós folhas.\n");
+//        PesqLeaf(arvore);
+//        resposta = Verifica(arvore, total);
+
+        printf("\n");
 
         testes -= 1;
 
     }
     return 0;
 }
+
+/*Falta retornar pra main que essas respotas impressas
+ * representam "Feliz Natal", caso não haja respostas => Hohoho!
+*/
+
+ int Verifica(TREEh arv, int total) {
+//Infixa
+//    if(arv->somaDir == 0 && arv->somaEsq == 0){
+//        Verifica(arv->esq, total);
+//        Verifica(arv->dir, total);
+//    }
+    if(arv) {
+        if (arv->somaEsq + arv->somaDir == total) {
+            return 0;
+        } else {
+            Verifica(arv->esq, total);
+            Verifica(arv->dir, total);
+            return 1;
+        }
+    }
+}
+
+
+/*int Verifica(TREEh arv, int total) {
+//Infixa
+    if (arv == NULL) {
+        return 0;
+    }
+    if(arv->somaEsq + arv->somaDir == total && abs(arv->somaEsq - arv->somaDir) <= 5){
+        printf("%i | %i\t", arv->somaEsq, arv->somaDir);
+        printf("//\tdiferenca: %d  \t",arv->somaEsq-arv->somaDir);
+        printf("// soma: %d\t//\t total: %d\n", arv->somaEsq + arv->somaDir, total);
+    }
+    if(arv->esq != NULL){
+        Verifica(arv->esq, total);
+    }
+    if(arv->dir != NULL){
+        Verifica(arv->dir, total);
+    }
+}*/
 
 void PesqIn(TREEh arv) {
 //Infixa
@@ -89,26 +138,6 @@ void PesqLeaf(TREEh arv) {
     }
 }
 
-int Verifica(TREEh arv) {
-//Infixa
-    if (arv == NULL) {
-        return;
-    }
-    if(arv->esq == NULL && arv->dir == NULL){
-        printf("\n%i | %i  ", arv->somaEsq, arv->somaDir);
-        printf(" // diferenca: %d  ",arv->somaEsq-arv->somaDir);
-        if(abs(arv->somaEsq - arv->somaDir) > 5){
-            printf(">> Caiu!");
-        }
-    }
-    if(arv->esq != NULL){
-        Verifica(arv->esq);
-    }
-    if(arv->dir != NULL){
-        Verifica(arv->dir);
-    }
-}
-
 
 void criaArvore(TREEh *arv, int vr) {
     if (*arv == NULL) {
@@ -117,9 +146,9 @@ void criaArvore(TREEh *arv, int vr) {
         (*arv)->somaDir = vr;
         (*arv)->esq = NULL;
         (*arv)->dir = NULL;
-        printf("Arvore criada com sucesso.\n");
+//        printf("Arvore criada com sucesso.\n");
     } else {
-        printf("Erro: Árvore já criada.\n");
+//        printf("Erro: Árvore já criada.\n");
     }
 }
 
@@ -133,11 +162,14 @@ void insArvore(TREEh *arv, int vr) {
 
 void insArvoreEsq(TREEh *arv, int vr, int esqPai, int dirPai) {
     if (*arv == NULL) {
-        *arv = (TREEh) malloc(sizeof(struct noh));
-        (*arv)->somaEsq = esqPai + vr;
-        (*arv)->somaDir = dirPai;
-        (*arv)->esq = NULL;
-        (*arv)->dir = NULL;
+        //printf("InsereEsquerda:\n vr: %d, esqPai: %d, dirPai: %d\n", vr, esqPai, dirPai);
+        if( abs(esqPai + vr - dirPai) <= 5) { //Condição para inserir apenas quando o balanço é atingido
+            *arv = (TREEh) malloc(sizeof(struct noh));
+            (*arv)->somaEsq = esqPai + vr;
+            (*arv)->somaDir = dirPai;
+            (*arv)->esq = NULL;
+            (*arv)->dir = NULL;
+        }
     } else {
         insArvoreEsq(&((*arv)->esq), vr, ((*arv)->somaEsq), ((*arv)->somaDir));
         insArvoreDir(&((*arv)->dir), vr, ((*arv)->somaEsq), ((*arv)->somaDir));
@@ -146,11 +178,14 @@ void insArvoreEsq(TREEh *arv, int vr, int esqPai, int dirPai) {
 
 void insArvoreDir(TREEh *arv, int vr, int esqPai, int dirPai) {
     if (*arv == NULL) {
-        *arv = (TREEh) malloc(sizeof(struct noh));
-        (*arv)->somaEsq = esqPai;
-        (*arv)->somaDir = dirPai + vr;
-        (*arv)->esq = NULL;
-        (*arv)->dir = NULL;
+        //printf("InsereDireita:\n vr: %d, esqPai: %d, dirPai: %d\n", vr, esqPai, dirPai);
+        if( abs(dirPai + vr - esqPai) <= 5) {
+            *arv = (TREEh) malloc(sizeof(struct noh));
+            (*arv)->somaEsq = esqPai;
+            (*arv)->somaDir = dirPai + vr;
+            (*arv)->esq = NULL;
+            (*arv)->dir = NULL;
+        }
     } else {
         insArvoreEsq(&((*arv)->esq), vr, ((*arv)->somaEsq), ((*arv)->somaDir));
         insArvoreDir(&((*arv)->dir), vr, ((*arv)->somaEsq), ((*arv)->somaDir));
